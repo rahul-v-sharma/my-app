88568f2d32c01b9051488ce838980e6886344935
Rahul Sharma <rh503546@dal.ca>
2024-03-18 18:22:02 -0300

    Chnages

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App">
+      <header className="App-header">
+        <h1>Hi, my name is Rahul Sharma</h1>
+        <p>This is my work for Lab 6 of Course CSCI 3172.</p>
+      </header>
+    </div>
       </header>
     </div>
   );
